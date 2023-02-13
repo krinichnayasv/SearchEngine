135ba6a94593e651e611d34235c00878bc797e48
krinichnayasv <122884382+krinichnayasv@users.noreply.github.com>
2023-02-13 16:08:22 +0300

    Add files via upload

---
+++ src/main/java/service/Morphology.java
@@ -0,0 +1,123 @@
+package service;
+
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.english.EnglishLuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Morphology {
+
+    private static final String[] SERVICE_WORDS_OF_SPEECH_RUS = {"МЕЖД", "ПРЕДЛ", "СОЮЗ", "ПРЕДК", "ЧАСТ"};
+    private static final String[] SERVICE_WORDS_OF_SPEECH_ENG = {"PN", "PREP", "PART", "ARTICLE", "CONJ"};
+    private static final String REG_RUS = "[А-Яа-я]+";
+    private static final String REG_ENG = "[A-Za-z]+";
+    private static final String REGEX = "[^A-Za-zА-Яа-я\\s]+";
+    private HashMap<String, Integer> lemmas = new HashMap<>();
+    private TreeMap<String, String> lemmasWords = new TreeMap<>();
+
+    private static LuceneMorphology lucMorphEng;
+    private static LuceneMorphology lucMorphRus;
+
+    static {
+        try {
+            lucMorphRus = new RussianLuceneMorphology();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static {
+        try {
+            lucMorphEng = new EnglishLuceneMorphology();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Morphology() {
+
+    }
+
+
+    public HashMap<String, Integer> getLemmas(String text) {
+        String[] words = text.toLowerCase().replaceAll(REGEX, " ").split("\\s+");
+
+        for (String word : words) {
+            if (word.matches(REG_RUS) && (word.trim().length() > 1 || word.trim().matches("я"))) {
+                List<String> wordInfo = lucMorphRus.getMorphInfo(word.trim());
+                for (String w : wordInfo) {
+                    List<String> wordBaseForms = !getRusInfo(w) ? lucMorphRus.getNormalForms(word.trim()) : new ArrayList<>();
+                    getLemmas(wordBaseForms);
+                }
+            } else if (word.matches(REG_ENG) && word.trim().length() > 1) {
+                List<String> wordInfo = lucMorphEng.getMorphInfo(word.trim());
+                for (String w : wordInfo) {
+                    List<String> wordBaseForms = !getEngInfo(w) ? lucMorphEng.getNormalForms(word.trim()) : new ArrayList<>();
+                    getLemmas(wordBaseForms);
+                }
+            }
+        }
+        return lemmas;
+    }
+
+    private void getLemmas (List<String> wordBaseForms) {
+        for (String lemma : wordBaseForms) {
+            if (lemmas.containsKey(lemma)) {
+                lemmas.put(lemma, lemmas.get(lemma) + 1);
+            } else {
+                lemmas.put(lemma, 1);
+            }
+        }
+    }
+
+    private  boolean getRusInfo(String word) {
+        HashSet<Boolean> wordsInfo = new HashSet<>();
+        for(int j=0; j<SERVICE_WORDS_OF_SPEECH_RUS.length; j++){
+            wordsInfo.add(word.contains(SERVICE_WORDS_OF_SPEECH_RUS[j]));}
+        return wordsInfo.contains(true);
+    }
+
+    private  boolean getEngInfo(String word) {
+        HashSet<Boolean> wordsInfo = new HashSet<>();
+        for(int j=0; j<SERVICE_WORDS_OF_SPEECH_ENG.length; j++){
+            wordsInfo.add(word.contains(SERVICE_WORDS_OF_SPEECH_ENG[j]));}
+        return wordsInfo.contains(true);
+    }
+
+
+    public TreeMap<String, String> getLemmaForWord(String[] words) {
+
+        for (String word : words) {
+            if (word.replaceAll(REGEX, "").trim().matches(REG_RUS)
+                    && (word.replaceAll(REGEX, "").trim().length() > 1
+                    || word.replaceAll(REGEX, "").trim().matches("я"))) {
+                List<String> wordInfo = lucMorphRus.getMorphInfo(word.replaceAll(REGEX, "").trim());
+                for (String w : wordInfo) {
+                    if (!getRusInfo(w)) {
+                        List<String> wordBaseForms = lucMorphRus.getNormalForms(word.replaceAll(REGEX, "").trim());
+                        getLemmasWords(wordBaseForms,word);
+                    }
+                }
+            } else if (word.replaceAll(REGEX, "").trim().matches(REG_ENG)
+                    && word.replaceAll(REGEX, "").trim().length() > 1) {
+                List<String> wordInfo = lucMorphEng.getMorphInfo(word.replaceAll(REGEX, "").trim());
+                for (String w : wordInfo) {
+                    if (!getEngInfo(w)) {
+                        List<String> wordBaseForms = lucMorphEng.getNormalForms(word.replaceAll(REGEX, "").trim());
+                        getLemmasWords(wordBaseForms, word);
+                    }
+                }
+            }
+        }
+        return lemmasWords;
+    }
+
+    private void getLemmasWords(List<String> wordBaseForms, String word) {
+        if (!lemmasWords.containsKey(word.trim())) {
+            lemmasWords.put(word.trim(), wordBaseForms.get(0));
+        }
+    }
+
+}
---
+++ src/main/java/service/ParseSiteOrPage.java
@@ -0,0 +1,119 @@
+package service;
+
+import main.config.ConfigData;
+import main.config.SiteConfig;
+import model.Site;
+import model.SiteStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+
+public class ParseSiteOrPage {
+
+    private static final String REGEX = "https?:\\/\\/[^,\\s]+";
+    private static ConfigData configData = new ConfigData();
+    private static List<SiteConfig>  sitesList = configData.getSites();
+    private static String userAgent = configData.getUserAgent();
+    private static String referrer = configData.getReferrer();
+    private static boolean isIndexing = false;
+    private static DBMethods dbMethods = SpringContext.getBean(DBMethods.class);
+    private static List<Task> tasks = new ArrayList<>();
+
+    public ParseSiteOrPage() {
+    }
+
+    public static List<Task> startIndexing() {
+        setIndexing(true);
+        dbMethods.deleteAllSites();
+
+        for(SiteConfig siteConfig : sitesList) {
+            Task task = new Task(siteConfig.getUrl().concat("/"), siteConfig.getName());
+            Thread thread = new Thread(task);
+            tasks.add(task);
+            thread.start();
+        }
+        return tasks;
+    }
+
+    public static void stopIndexing(){
+        getTasks().stream().forEach(t -> t.stop());
+    }
+
+    public static List<Task> getTasks() {
+        return tasks;
+    }
+
+    public static void reIndexingPage(String url, String siteUrl) {
+        Runnable run = () -> {
+            Site site = dbMethods.getSiteByUrl(siteUrl);
+            service.Node root = new service.Node(url, "");
+            service.IndexingSites map = new service.IndexingSites(root, site, dbMethods);
+            ForkJoinPool pool = new ForkJoinPool();
+            pool.invoke(map);
+        };
+        Thread thread = new Thread(run);
+        thread.start();
+    }
+
+    public static List<Task> reIndexingSite(SiteConfig siteConfig) {
+        Task task = new Task(siteConfig.getUrl().concat("/"), siteConfig.getName());
+        Thread thread = new Thread(task);
+        tasks.add(task);
+        thread.start();
+        return tasks;
+    }
+
+    public static boolean checkUrlForReindexing(String url) {
+        boolean checked = false;
+        if (!url.matches(REGEX)) {
+            checked = false;
+        } else {
+            for(SiteConfig siteConfig : sitesList) {
+                if(url.matches(siteConfig.getUrl())) {
+                    checked = true;
+                    reIndexingSite(siteConfig);
+                    break;
+                } else if(url.startsWith(siteConfig.getUrl())) {
+                    checked = true;
+                    reIndexingPage(url, siteConfig.getUrl());
+                    break;
+                }
+            }
+        }
+        return checked;
+    }
+
+    public static boolean getIndexing() {
+        List<Site> sites =  dbMethods.getAllSites();
+        for(Site site : sites) {
+            if(site.getStatus() == SiteStatus.INDEXING) {
+                isIndexing = true;
+                break;
+            } else {
+                isIndexing = false;
+            }
+        }
+        return isIndexing;
+    }
+
+    public static SearchByQuery searchByQuery(String urlSite, String query) {
+       return new SearchByQuery(query, urlSite, dbMethods);
+    }
+
+    public static String getUserAgent() {
+        return userAgent;
+    }
+
+    public static String getReferrer() {
+        return referrer;
+    }
+
+    public static void setIndexing(boolean indexing) {
+        isIndexing = indexing;
+    }
+
+
+
+}
